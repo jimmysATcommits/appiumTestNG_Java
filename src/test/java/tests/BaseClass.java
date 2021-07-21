53a07cc381ebe5e67ec85edd0dc7e0295807477d
Jimmy Ru <jru@toddcorporation.com>
2021-07-22 09:15:37 +1200

    add more files

---
+++ src/test/java/tests/BaseClass.java
@@ -0,0 +1,52 @@
+package tests;
+
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class BaseClass {
+AppiumDriver<MobileElement> driver;
+	
+@BeforeTest
+public void setup()
+{
+ try
+ {
+  DesiredCapabilities caps= new DesiredCapabilities();
+  caps.setCapability(MobileCapabilityType.PLATFORM_NAME,"ANDROID");
+  caps.setCapability(MobileCapabilityType.PLATFORM_VERSION,"8.1.0");
+  caps.setCapability(MobileCapabilityType.DEVICE_NAME,"Samsung J2 Pro");
+  caps.setCapability(MobileCapabilityType.UDID,"7dced4d9");
+  caps.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT,60);
+  //caps.setCapability("appPackage","com.google.android.calculator");
+  //caps.setCapability("appActivity","com.android.calculator2.Calculator");
+  caps.setCapability("appPackage","com.sec.android.app.popupcalculator");
+  caps.setCapability("appActivity","com.sec.android.app.popupcalculator.Calculator");
+  URL url = new URL("http://0.0.0.0:4723/wd/hub");
+  driver = new AppiumDriver<MobileElement>(url, caps);
+  }catch(Exception exp)
+  {}
+ }
+ @Test
+ public void sampleTest()
+ {
+  System.out.println("I am inside sample Test");
+ }
+ 
+ 
+ @AfterTest
+ public void teardown()
+ {
+	 
+ }
+}
